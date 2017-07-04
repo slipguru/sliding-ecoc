693a4131dbf921023ac19217e912b4f81e433cf0
Federico Tomasi <fdtomasi@gmail.com>
2017-07-04 18:34:37 +0200

    add main loop and random label split

--- secoc/estimator.py
+++ secoc/estimator.py
@@ -13,6 +13,7 @@ from sklearn.ensemble.base import BaseEnsemble
 from sklearn.externals import joblib as jl
 from sklearn.externals.six.moves import zip as izip
 from sklearn.multiclass import _check_estimator, _fit_binary, _predict_binary
+from sklearn.preprocessing import LabelBinarizer
 from sklearn.utils import check_random_state
 from sklearn.utils.validation import check_is_fitted
 
@@ -23,12 +24,23 @@ from sklearn.utils.multiclass import (_check_partial_fit_first_call,
 from sklearn.utils.metaestimators import _safe_split
 
 
+def random_binarizer(y):
+    y = np.array(y)
+    unique = np.unique(y)
+    np.random.shuffle(unique)
+    xx = unique[:np.random.randint(1, unique.size - 1)]
+    return reduce(np.logical_or, [np.array(y) == x for x in xx]).astype(int)
+
+
 class SlidingECOC(BaseEnsemble, ClassifierMixin, MetaEstimatorMixin):
 
-    def __init__(self, base_estimator=None, n_estimators=50,
-                 code_size=1.5,  # division on the number of classes
-                 window_size=5,
+    def __init__(self, base_estimator=None,
+                 n_estimators=50,
                  estimator_params=tuple(),  # ?
+                 window_size=5,
+                 # code_size=,  # division on the number of classes
+                 oob_score=False,
+                 max_features=None,
                  random_state=None, n_jobs=1):
 
         super(SlidingECOC, self).__init__(
@@ -36,11 +48,13 @@ class SlidingECOC(BaseEnsemble, ClassifierMixin, MetaEstimatorMixin):
             n_estimators=n_estimators,
             estimator_params=estimator_params)
 
-        self.base_estimator = base_estimator
-        self.code_size = code_size
+        # self.base_estimator = base_estimator
+        # self.code_size = code_size
+        self.window_size = window_size
+        self.oob_score = oob_score
+        self.max_features = max_features
         self.random_state = random_state
         self.n_jobs = n_jobs
-        self.window_size = window_size
 
     def fit(self, X, y):
         """Fit underlying estimators.
@@ -57,10 +71,15 @@ class SlidingECOC(BaseEnsemble, ClassifierMixin, MetaEstimatorMixin):
         -------
         self
         """
-        if self.code_size <= 0:
-            raise ValueError("code_size should be greater than 0, got {1}"
-                             "".format(self.code_size))
-
+        # if self.code_size <= 0:
+        #     raise ValueError("code_size should be greater than 0, got {1}"
+        #                      "".format(self.code_size))
+        if self.max_features is None:
+            self.max_features = self.window_size
+        elif not 1 < self.max_features <= self.window_size:
+            raise ValueError(
+                "max_features must be included in [2, window_size], got {0}"
+                " instead.".format(self.max_features))
         _check_estimator(self.base_estimator)
         random_state = check_random_state(self.random_state)
 
@@ -68,26 +87,47 @@ class SlidingECOC(BaseEnsemble, ClassifierMixin, MetaEstimatorMixin):
         n_classes = self.classes_.shape[0]
         code_size_ = int(n_classes * self.code_size)
 
+        n_samples, n_features = X.shape
+
         # FIXME: there are more elaborate methods than generating the codebook
         # randomly.
-        self.code_book_ = random_state.random_sample((n_classes, code_size_))
-        self.code_book_[self.code_book_ > 0.5] = 1
+        # self.code_book_ = random_state.random_sample((n_classes, code_size_))
+        # self.code_book_[self.code_book_ > 0.5] = 1
+        #
+        # if hasattr(self.base_estimator, "decision_function"):
+        #     self.code_book_[self.code_book_ != 1] = -1
+        # else:
+        #     self.code_book_[self.code_book_ != 1] = 0
+        #
+        # classes_index = dict((c, i) for i, c in enumerate(self.classes_))
+        #
+        # Y = np.array([self.code_book_[classes_index[y[i]]]
+        #               for i in range(X.shape[0])], dtype=np.int)
 
-        if hasattr(self.base_estimator, "decision_function"):
-            self.code_book_[self.code_book_ != 1] = -1
-        else:
-            self.code_book_[self.code_book_ != 1] = 0
+        # sliding window
+        self.estimator_features_ = []
+        for start in range(n_features):
+            features = []
+            y_binary_splits = []
+            for p in range(self.n_estimators):
+                # prepare the features
+                # 1. choose randomly the max features belonging to [0, k-1]
+                # 1a. how many?
+                n_features_window = np.random.randint(0, self.max_features)
+                # 1b. which ones?
+                features.append(
+                    np.random.randint(0, self.window_size, n_features_window)
+                    + start)
 
-        classes_index = dict((c, i) for i, c in enumerate(self.classes_))
+                # 2. split y and binarise it
+                y_binary_splits.append(random_binarizer(y))
 
-        Y = np.array([self.code_book_[classes_index[y[i]]]
-                      for i in range(X.shape[0])], dtype=np.int)
-
-        return self, Y  # debug
-
-        self.estimators_ = jl.Parallel(n_jobs=self.n_jobs)(
-            jl.delayed(_fit_binary)(self.base_estimator, X, Y[:, i])
-            for i in range(Y.shape[1]))
+            self.estimators_ = jl.Parallel(n_jobs=self.n_jobs)(
+                jl.delayed(_fit_binary)(
+                    self.base_estimator, X[:, feats], y_binary)
+                for feats, y_binary in zip(features, y_binary_splits))
+            self.estimator_features_.extend(features)
+            self.estimator_splits_.extend(y_binary_splits)
 
         return self
 
@@ -105,6 +145,6 @@ class SlidingECOC(BaseEnsemble, ClassifierMixin, MetaEstimatorMixin):
             Predicted multi-class targets.
         """
         check_is_fitted(self, 'estimators_')
-        Y = np.array([_predict_binary(e, X) for e in self.estimators_]).T
-        pred = euclidean_distances(Y, self.code_book_).argmin(axis=1)
-        return self.classes_[pred]
+        # Y = np.array([_predict_binary(e, X) for e in self.estimators_]).T
+        # pred = euclidean_distances(Y, self.code_book_).argmin(axis=1)
+        # return self.classes_[pred]
